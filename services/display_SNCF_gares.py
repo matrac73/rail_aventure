fa1da838e4d49e273ddee7764750316bcdcbc723
Mathieu Ract <m.ract@groupeonepoint.com>
2024-03-24 22:02:21 +0100

    ménage

--- services/display_SNCF_gares.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("./data/SNCF_gares.csv", encoding="ISO-8859-1")
-
-lng_var = df[(df['lat'] > 35) & (df['lat'] < 60)]["lon"].tolist()
-lat_var = df[(df['lat'] > 35) & (df['lat'] < 60)]["lat"].tolist()
-plt.scatter(x=lng_var, y=lat_var, marker="o")
-plt.show()
